1a6a35a8cf9b181b97abe0023640a37e0aa9138d
[Suhyeon] <[cjc2929@naver.com]>
2022-10-28 15:27:38 +0900

    간단버전

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import ReactDOM from 'react-dom/client';
 
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+root.render(
+  <>
+    <h1>가위바위보</h1>
+    <h2>{getResult(me, other)}</h2>
+    <button onClick={handleClick}>가위</button>
+    <button onClick={handleClick}>바위</button>
+    <button onClick={handleClick}>보</button>
+  </>
+);
